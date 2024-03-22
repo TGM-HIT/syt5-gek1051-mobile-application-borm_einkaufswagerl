0bd2b45ed96e5f57603815a93fb0f7ccbeeb6c42
Can Polat <74594491+cpolat-tgm@users.noreply.github.com>
2024-03-22 08:39:53 +0100

    test added

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -1,20 +1,37 @@
-// Starts local server
-describe('template spec', () => {
-  it('should toggle checkbox state to true on click', () => {
-    // Replace YOUR_PAGE_URL with the URL of the page you're testing
+describe('Mark item as bought', function () {
+
+  it('Should create a new item and mark it as bought', function () {
     cy.visit('127.0.0.1:8081');
 
-    // Assuming clicking the checkbox toggles its state,
-    // and that it was initially unchecked.
-    // First, ensure the checkbox is not checked.
-    cy.get('div.md-checkbox-container')
-      .should('not.be.checked');
 
-    // Click the checkbox or button
-    cy.get('div.md-checkbox-container').click();
+    const listName = 'Getränke';
+    const itemName = 'Cola';
 
-    // Assert the checkbox is now checked
-    cy.get('md-checkbox-container')
-      .should('be.checked');
+    cy.get('#addNewList').click();
+    cy.get('#listName').type(listName);
+    cy.get('#addList').click();
+
+    // Wait for the new list to be added to the DOM
+    cy.contains('.main-content', listName, { timeout: 10000 }).should('exist');
+
+    // Click the list item addShoppingListItem button
+    cy.get('.main-content').contains(listName).parent().parent().
+      find('#addShoppingListItem').click();
+
+    // Type the item name 
+    cy.get('.main-content').find('#newItemName').type(itemName);
+
+    // Click the add button
+    cy.get('.main-content').find('#addToCart').click();
+
+    // Check that the item is added to the list
+    cy.get('.main-content').contains(itemName).should('exist');
+
+    // Mark the item as marked and check that it is marked
+    cy.get('.main-content').contains(itemName).parent().parent().
+      find('input[type="checkbox"]').
+      scrollIntoView().
+      check({ force: true }).
+      should('have.value', 'true');
   });
-})
+});
